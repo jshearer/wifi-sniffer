da34cf88768f2effe1266c9490696233ca9d0f97
jshearer <joseph@josephshearer.net>
2014-12-29 14:33:08 -0500

    Fixed receiver

--- sniff_data_receiver.py
+++ sniff_data_receiver.py
@@ -12,7 +12,10 @@ def start_listening():
 
 	while True:
 		#Poll for messages. If they show up, it'll call new_datapoint
-		new_datapoint(redis_queue.get_message())
+		msg = redis_queue.get_message()
+		while msg is not None:
+			new_datapoint(msg)
+			msg = redis_queue.get_message()
 		time.sleep(0.0001)
 
 if __name__ == "__main__":
