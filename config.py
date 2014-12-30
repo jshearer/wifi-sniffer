ad2b5059938e021eeff497fa97c457168884cab3
jshearer <joseph@josephshearer.net>
2014-12-30 12:55:32 -0500

    There's a bug in the wifi module that doesn't allow for spaces in SSIDs. So I renamed mine...

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import redis
 #Alfa MAC addr prefix
 sniffer_mac_beginning = ['00:c0:ca']
-real_network = 'Shearer wireless'
+real_network = 'ShearerWireless'
 DEBUG=True
 
 root_dir = '/home/joseph/sniffer/'
