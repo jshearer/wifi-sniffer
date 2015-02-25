0845839a5742db3c747a659938592f1bf5e6eaa2
Joseph <joseph@josephshearer.net>
2015-02-25 14:28:01 -0500

    Get device id from __init__

--- WiLoc/communication/api.py
+++ WiLoc/communication/api.py
@@ -5,6 +5,7 @@ import json
 import time
 
 from WiLoc.config import server_address
+from WiLoc import device_id
 
 transmitter_mapping = {}
 receiver_mapping = {}
@@ -107,8 +108,8 @@ def new_recording(transmitter,receiver,rssi):
 		logging.info("Flushing %i recordings! elapsedtime: %f"%(len(recording_buffer),time.time()-last_req,))
 		flush_recordings()
 
-def get_host_id(device_id):
-	server_query = get('hosts',{'device_uid':device_id})['results']
+def get_host_id(dev_id):
+	server_query = get('hosts',{'device_uid':dev_id})['results']
 
 	if len(server_query)==1:
 		return server_query[0]['url']
