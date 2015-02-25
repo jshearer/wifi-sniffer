d8481502d04f287024087375885548702da6979e
Joseph <joseph@josephshearer.net>
2015-02-25 16:16:24 -0500

    Changed the paths a bit.

--- WiLoc/config.py
+++ WiLoc/config.py
@@ -1,6 +1,6 @@
 #Alfa MAC addr prefix
 
-server_address = "http://wiloc.josephshearer.net"
+server_address = "http://wiloc.josephshearer.net/api"
 sniffer_mac_beginning = ['00:c0:ca']
 wifi_backup_filename = "wifi_data.json"
 
