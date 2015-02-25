6bdbeedc59e61f09b59971e51136c82a70aff652
Joseph <joseph@josephshearer.net>
2015-02-25 17:15:11 -0500

    Api v1

--- WiLoc/config.py
+++ WiLoc/config.py
@@ -1,6 +1,6 @@
 #Alfa MAC addr prefix
 
-server_address = "http://wiloc.josephshearer.net/api/"
+server_address = "http://wiloc.josephshearer.net/api/v1/"
 sniffer_mac_beginning = ['00:c0:ca']
 wifi_backup_filename = "wifi_data.json"
 
