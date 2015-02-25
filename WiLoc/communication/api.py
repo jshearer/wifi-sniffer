0dcd02d7e348e262d9b36cf5ee42e44bb84837f8
Joseph <joseph@josephshearer.net>
2015-02-24 22:32:01 -0500

    Authentication works! Woot!

--- WiLoc/communication/api.py
+++ WiLoc/communication/api.py
@@ -13,18 +13,36 @@ req_every = 1
 req_size = 400
 last_req = time.time()
 
+session = requests.Session()
+
+auth_info = {
+	'username': 'wiloc_admin',
+	'password': 'wiloc_admin'
+}
+
+def fix_url(url):
+	return (url if url.endswith('/') else url + '/')
+
+auth_url = fix_url(urlparse.urljoin(server_address,'api-auth/login?next=/'))
+
+#get csrf token
+csrf = session.get(auth_url).cookies['csrftoken']
+
+#Authenticate
+session.post(auth_url, data=auth_info, headers={'X-CSRFToken':csrf})
+
 def get(resource, params=dict()):
 
-	params.update({"format":"json"})
+	params.update({'format':'json'})
 	headers = {'Content-type': 'application/json'}
 
 	url = urlparse.urljoin(server_address,resource)
 	if not url.endswith('/'):
 		url = url + '/'
 	
-	return requests.get(url,params=params, headers=headers).json()
+	return session.get(url,params=params, headers=headers).json()
 
-def post(resource, params=dict(), data=dict()):
+def post(resource, params=dict(), data=dict(), run_json=True):
 
 	headers = {'Content-type': 'application/json'}
 
@@ -32,9 +50,9 @@ def post(resource, params=dict(), data=dict()):
 	if not url.endswith('/'):
 		url = url + '/'
 
-	response = requests.post(url,params=params, data=json.dumps(data), headers=headers)
+	response = session.post(url,params=params, data=json.dumps(data), headers=headers)
 
-	return response.json()
+	return (response.json() if run_json else response)
 
 def get_transmitter_id(mac_addr):
 	if mac_addr in transmitter_mapping:
@@ -65,7 +83,7 @@ def get_receiver_id(mac_addr):
 			receiver_mapping[mac_addr] = server_query[0]['url']
 			return server_query[0]['url']
 	except requests.exceptions.ConnectionError as e:
-		logging.error("Unable to connect to server to get receiver id.")
+		logging.error('Unable to connect to server to get receiver id.')
 
 	return None
 
@@ -89,12 +107,12 @@ def new_recording(transmitter,receiver,rssi):
 	receiver_id = get_receiver_id(receiver)
 
 	if receiver_id is None:
-		raise Exception("Receiver not in database. Please add: "+str(receiver))
+		raise Exception('Receiver not in database. Please add: '+str(receiver))
 
 	recording_buffer.append({'transmitter':transmitter_id,'receiver':receiver_id,'rssi':rssi})
 	
 	if (time.time()-last_req > req_every) or len(recording_buffer)>req_size:
-		logging.info("Flushing %i recordings! elapsedtime: %f"%(len(recording_buffer),time.time()-last_req,))
+		logging.info('Flushing %i recordings! elapsedtime: %f'%(len(recording_buffer),time.time()-last_req,))
 		flush_recordings()
 
 def get_host_id(device_id):
