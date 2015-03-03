3c9184791563748bab4b76a4ba75f0171452aa83
Joseph <joseph@josephshearer.net>
2015-03-02 19:38:04 -0500

    Exit if critical error.

--- WiLoc/__init__.py
+++ WiLoc/__init__.py
@@ -56,5 +56,6 @@ def log_uncaught_exceptions(ex_cls, ex, tb):
 
 	logging.critical(''.join(traceback.format_tb(tb)))
 	logging.critical('{0}: {1}'.format(ex_cls, ex))
+	sys.exit(0)
 
 sys.excepthook = log_uncaught_exceptions
