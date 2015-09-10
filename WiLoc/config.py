db60bf4b650a8f4bba581f1cf9ed213a3cc70ca4
Joseph Shearer <joseph@josephshearer.net>
2015-09-10 15:44:35 -0400

    Fix typo

--- WiLoc/config.py
+++ WiLoc/config.py
@@ -1,6 +1,6 @@
 #Alfa MAC addr prefix
 
-server_address = "http://jasmpb.student.rit.edu:9001/api/v1/"
+server_address = "http://jasmbp.student.rit.edu:9001/api/v1/"
 # graylog_address = "logging.josephshearer.net"
 # graylog_port = 12205
 sniffer_mac_beginning = ['00:c0:ca']
